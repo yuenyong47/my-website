c53083b1415712e03323e4b3466985dd03300823
yuenyong47 <yuenyong47@gmail.com>
2025-03-11 15:26:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    Swal.fire({
+      title: 'สวัสดีครับนักเรียน',
+      text: 'ยินดีต้อนรับสู่เว็บไซต์ของเรา!',
+      icon: 'success', // หรือ 'info', 'warning', 'error'
+      confirmButtonText: 'ตกลง'
+    });
+  }
+  
